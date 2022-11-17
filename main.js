50a32abd0c830ade9ac72e0e096c772b0a3a09a9
soyUnaGata <fallen.snitch@gmail.com>
2022-11-17 13:28:46 +0200

    refactoring some css and js

--- main.js
+++ main.js
@@ -2,8 +2,20 @@ const rows = document.querySelectorAll('.row');
 const whoIsWinner = document.querySelector('.players_t');
 const playerX = document.querySelector('.player_x');
 const playerO = document.querySelector('.player_o'); 
+
+const players = [
+    {
+        element: document.querySelector('.player_x'),
+        classSymbol: 'x-symbol'
+    },
+    {
+        element: document.querySelector('.player_o'),
+        classSymbol: 'o-symbol'
+    }
+];
+
 const arrayOfRows = [];
-let counter = 0;
+let moveNumber = 0;
 let hasWinner = false;
 for(let i = 0; i < rows.length; i++){
     const elem = rows[i];
@@ -70,22 +82,18 @@ function render (e){
     if(hasWinner) return;
     if(elem.innerHTML)
         return 
-    counter++;
-    if(counter % 2 == 1){
-        elem.innerHTML = playerX.innerHTML;
-        elem.style.color = 'rgb(175,179,214)'; 
-        elem.style.textShadow = ' 0 0 5px rgb(66, 110, 211), 0 0 15px rgb(66, 110, 211), 0 0 20px rgb(66, 110, 211), 0 0 35px rgb(49, 37, 142), 0 0 40px rgb(49, 37, 142), 0 0 50px rgb(49, 37, 142)';
-        elem.style.fontSize = '70px';
-    }else{
-        elem.innerHTML = playerO.innerHTML;
-        elem.style.fontSize = '70px';
-        elem.style.color = 'rgb(248, 211, 203)'; 
-        elem.style.textShadow = ' 0 0 5px rgb(178, 80, 72), 0 0 15px rgb(178, 80, 72), 0 0 20px rgb(178, 80, 72), 0 0 35px rgb(181, 42, 56), 0 0 40px rgb(181, 42, 56), 0 0 50px rgb(181, 42, 56)';
-    }
+    const playerIndex = moveNumber % 2;
+    fillCellAndStyle(elem, players[playerIndex].element.innerHTML, players[playerIndex].classSymbol);
+    moveNumber++;
     checkWinner()
-    if( counter == 9){
+    if( moveNumber == 9){
         whoIsWinner.innerHTML = `We don't have a winner today`;  
     }
+}
+
+function fillCellAndStyle(cellElement, symbol, classSymbol){
+    cellElement.innerHTML = symbol;
+    cellElement.classList.add(classSymbol);
 }
 
 function hideLosser(losser){
